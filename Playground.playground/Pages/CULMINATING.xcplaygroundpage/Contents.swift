9dc37656eea7c822eafa1d9b4e6052a5828d045c
Ben Stainrod <BStainrod@lcs.on.ca>
2020-12-11 09:15:52 -0500

    Finished Culminating

--- Playground.playground/Pages/CULMINATING.xcplaygroundpage/Contents.swift
+++ Playground.playground/Pages/CULMINATING.xcplaygroundpage/Contents.swift
@@ -47,29 +47,33 @@ func drawShape(){
     turtle.forward(steps: 20)
     turtle.left(by: 45)
     turtle.forward(steps: 60)
+
+    
+    
 }
 //moving turtle into place
 turtle.penUp()
 turtle.left(by: 90)
-turtle.forward(steps: 200)
+turtle.forward(steps: 100)
 turtle.right(by: 90)
 //function to loop it
 turtle.penDown()
-for _ in 1...7 {
+for _ in 1...8 {
     turtle.penDown()
+    
     drawShape()
     turtle.penUp()
 turtle.right(by: 90)
 turtle.forward(steps: 77)
 }
 // setting up for second row
-turtle.backward(steps: 539)
+turtle.backward(steps: 616)
 turtle.penUp()
 turtle.left(by: 90)
 turtle.forward(steps: 89)
 turtle.right(by: 90)
 //starting second row
-for _ in 1...7 {
+for _ in 1...8 {
     turtle.penDown()
     drawShape()
     turtle.penUp()
@@ -77,13 +81,13 @@ turtle.right(by: 90)
 turtle.forward(steps: 77)
 }
 // setting up for third row
-turtle.backward(steps: 539)
+turtle.backward(steps: 616)
 turtle.penUp()
 turtle.left(by: 90)
 turtle.forward(steps: 89)
 turtle.right(by: 90)
 //starting third row
-for _ in 1...7 {
+for _ in 1...8 {
     turtle.penDown()
     drawShape()
     turtle.penUp()
@@ -91,13 +95,13 @@ turtle.right(by: 90)
 turtle.forward(steps: 77)
 }
 // setting up for fourth row
-turtle.backward(steps: 539)
+turtle.backward(steps: 616)
 turtle.penUp()
 turtle.left(by: 90)
 turtle.forward(steps: 89)
 turtle.right(by: 90)
 //starting fourth row
-for _ in 1...7 {
+for _ in 1...8 {
     turtle.penDown()
     drawShape()
     turtle.penUp()
@@ -105,16 +109,46 @@ turtle.right(by: 90)
 turtle.forward(steps: 77)
 }
 // setting up for fifth row
-turtle.backward(steps: 539)
+turtle.backward(steps: 616)
 turtle.penUp()
 turtle.left(by: 90)
 turtle.forward(steps: 89)
 turtle.right(by: 90)
 //starting fifth row
-for _ in 1...7 {
+for _ in 1...8 {
     turtle.penDown()
     drawShape()
     turtle.penUp()
 turtle.right(by: 90)
 turtle.forward(steps: 77)
 }
+// setting up for sixth row
+turtle.backward(steps: 616)
+turtle.penUp()
+turtle.left(by: 90)
+turtle.forward(steps: 89)
+turtle.right(by: 90)
+//starting sixth row
+for _ in 1...8 {
+    turtle.penDown()
+    drawShape()
+    turtle.penUp()
+turtle.right(by: 90)
+turtle.forward(steps: 77)
+}
+// setting up for seventh row
+turtle.backward(steps: 616)
+turtle.penUp()
+turtle.left(by: 90)
+turtle.forward(steps: 89)
+turtle.right(by: 90)
+//starting seventh row
+for _ in 1...8 {
+    turtle.penDown()
+    drawShape()
+    turtle.penUp()
+turtle.right(by: 90)
+turtle.forward(steps: 77)
+}
+
+turtle.copySVGToClipboard()
